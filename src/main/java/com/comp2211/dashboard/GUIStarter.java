165d51182196fcd678a91b1a3e33cec02626b213
Charles Yim <charlesyim1999@gmail.com>
2020-03-03 15:21:17 +0000

    Added GUIStarter to fix .jar problem

---
+++ src/main/java/com/comp2211/dashboard/GUIStarter.java
@@ -0,0 +1,11 @@
+package com.comp2211.dashboard;
+
+import com.comp2211.dashboard.view.App;
+
+public class GUIStarter {
+
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+
+}
