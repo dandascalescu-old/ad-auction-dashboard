fb5d927abd19ec7f0b3115ad714b480050b7f893
freddieb <freddieiabrown@hotmail.co.uk>
2020-03-04 11:04:53 +0000

    Remove old MVC controllers and HelloWorld example

--- src/main/java/com/comp2211/dashboard/view/App.java
+++ src/main/java/com/comp2211/dashboard/view/App.java
@@ -1,14 +1,9 @@
 package com.comp2211.dashboard.view;
 
-import com.comp2211.dashboard.Campaign;
-import com.comp2211.dashboard.io.DatabaseManager;
-import com.comp2211.dashboard.viewmodel.PrimaryController;
 import com.comp2211.dashboard.viewmodel.PrimaryViewModel;
 import de.saxsys.mvvmfx.FluentViewLoader;
 import de.saxsys.mvvmfx.ViewTuple;
-import eu.lestard.doc.VisibleForTesting;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -22,32 +17,7 @@ public class App extends Application {
 
   @Override
   public void start(Stage stage) throws IOException {
-//    DatabaseManager.init();
-//    Campaign campaign = new Campaign("1");
-//    campaign.cacheData(0);
-//    System.out.println(campaign.getTotalCost());
-//
-//    FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("PrimaryView.fxml"));
-//    Parent root = fxmlLoader.load();
-//    PrimaryController primaryController = (PrimaryController) fxmlLoader.getController();
-//
-//    System.out.println(primaryController);
-//    System.out.println(campaign);
-//
-//    primaryController.setCampaign(campaign);
-//
-//    //primaryController.setGraphValue(campaign.getDateAverages());
-//
-//    // Parent root2 = FXMLLoader.load(getClass().getResource("testScene1.fxml"));
-//    Scene scene = new Scene(root);
-//
-//    primaryStage.setScene(scene);
-//    // primaryStage.initStyle(StageStyle.DECORATED);
-//
-//    primaryStage.setScene(scene);
-//    primaryStage.show();
-//
-//    stage.setTitle("Hello World Application");
+    stage.setTitle("Ad Analytics Dashboard");
 
     ViewTuple<PrimaryView, PrimaryViewModel> viewTuple = FluentViewLoader.fxmlView(PrimaryView.class).load();
 
