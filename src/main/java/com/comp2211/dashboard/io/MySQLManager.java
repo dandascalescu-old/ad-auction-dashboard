6ceed07e9ee87ae3a51936df1b1e0f70d189afb7
djd2g18 <djd2g18@soton.ac.uk>
2020-05-06 14:24:03 +0100

    Queries to impression_table no longer join the table to itself.

--- src/main/java/com/comp2211/dashboard/io/MySQLManager.java
+++ src/main/java/com/comp2211/dashboard/io/MySQLManager.java
@@ -193,7 +193,7 @@ public class MySQLManager extends DatabaseManager {
    */
   @Override
   public BigDecimal retrieveTotalCost(Cost type, Filter filter) {
-    String where = filterToWhere(filter, Table.impression_table);
+    String where = filterToWhere(filter, (type.equals(Cost.Click_Cost) ? Table.click_table : Table.impression_table));
     String statement = "SELECT SUM(" + type.toString() + ") AS SUM " +
             "FROM " + (type.equals(Cost.Click_Cost) ? click_table : impression_table) +
             ( where.isEmpty() ? "" : " WHERE " + where );
@@ -270,7 +270,7 @@ public class MySQLManager extends DatabaseManager {
    */
   @Override
   public HashMap<String, BigDecimal> retrieveDatedAverageCost(Cost type, Filter filter) {
-    String where = filterToWhere(filter, Table.impression_table);
+    String where = filterToWhere(filter, (type.equals(Cost.Click_Cost) ? Table.click_table : Table.impression_table));
     String statement = "SELECT DATE(Date) AS DateOnly, AVG(" + type.toString() + ") AS AVG " +
             "FROM " + (type.equals(Cost.Click_Cost) ? click_table : impression_table) +
             ( where.isEmpty() ? "" : " WHERE " + where ) +
@@ -491,7 +491,7 @@ public class MySQLManager extends DatabaseManager {
     if (table.equals(Table.server_table)) dateTitle = "Entry_Date";
 
     String where = "";
-    where += (filter.startDate != null ? (where.isEmpty() ? "" : " AND ") + "DATE(" + dateTitle + ") >= '" + filter.startDate.toString() + "'" : "");
+    where += (filter.startDate != null ?                                    "DATE(" + dateTitle + ") >= '" + filter.startDate.toString() + "'" : "");
     where += (filter.endDate   != null ? (where.isEmpty() ? "" : " AND ") + "DATE(" + dateTitle + ") <= '" + filter.endDate.toString()   + "'" : "");
 
     String ID = "";
@@ -499,12 +499,9 @@ public class MySQLManager extends DatabaseManager {
     ID += (filter.age     >= 0 ? (ID.isEmpty() ? "" : " AND ") + "Age = "     + filter.age     : "");
     ID += (filter.income  >= 0 ? (ID.isEmpty() ? "" : " AND ") + "Income = "  + filter.income  : "");
     ID += (filter.context >= 0 ? (ID.isEmpty() ? "" : " AND ") + "Context = " + filter.context : "");
-    /*where += (filter.gender    >= 0    ? (where.isEmpty() ? "" : " AND ") + "ID IN (SELECT DISTINCT ID FROM " + Table.impression_table + " WHERE Gender = "  + filter.gender  + ")" : "");
-    where += (filter.age       >= 0    ? (where.isEmpty() ? "" : " AND ") + "ID IN (SELECT DISTINCT ID FROM " + Table.impression_table + " WHERE Age = "     + filter.age     + ")" : "");
-    where += (filter.income    >= 0    ? (where.isEmpty() ? "" : " AND ") + "ID IN (SELECT DISTINCT ID FROM " + Table.impression_table + " WHERE Income = "  + filter.income  + ")" : "");
-    where += (filter.context   >= 0    ? (where.isEmpty() ? "" : " AND ") + "ID IN (SELECT DISTINCT ID FROM " + Table.impression_table + " WHERE Context = " + filter.context + ")" : "");*/
 
-    ID = (ID.isEmpty() ? ID : "ID IN (SELECT DISTINCT ID FROM " + Table.impression_table + " WHERE " + ID + ")");
+    if (!table.equals(Table.impression_table))
+      ID = (ID.isEmpty() ? ID : "ID IN (SELECT DISTINCT ID FROM " + Table.impression_table + " WHERE " + ID + ")");
     where = (where.isEmpty() ? (ID.isEmpty() ? "" : ID) : (ID.isEmpty() ? where : where + " AND " + ID));
     where += (where.isEmpty() ? " Campaign_ID = "  + filter.getCampaignID() : " AND Campaign_ID = " + filter.getCampaignID());
     return where;
