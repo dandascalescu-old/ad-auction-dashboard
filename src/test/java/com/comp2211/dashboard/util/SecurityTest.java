30a6d74aee3bb17529683befb287f2a94b56d050
freddieb <freddieiabrown@hotmail.co.uk>
2020-05-13 09:13:25 +0100

    Add test annotations

--- src/test/java/com/comp2211/dashboard/util/SecurityTest.java
+++ src/test/java/com/comp2211/dashboard/util/SecurityTest.java
@@ -17,12 +17,11 @@ class SecurityTest {
   void tearDown() {}
 
   @Test
-  @Description("Validation: Test password encoding and matching")
+  @Description("34 Validation: Test password encoding and matching")
   void encodePasswordMatched() {
     String testPassword = "hilbwqeq8432h;3pf23j94p";
     byte[] testSalt = "giueofuiwe45687y38hxftxb".getBytes();
 
-    ResultSet rs = null;
     byte[] hash = Security.encodePassword(testPassword, testSalt);
 
     Boolean passwordMatched = Security.matchPassword(testPassword, hash, testSalt);
@@ -31,13 +30,12 @@ class SecurityTest {
   }
 
   @Test
-  @Description("Defect test: Test different passwords")
+  @Description("35 Defect test: Test different passwords")
   void encodePasswordNotMatched() {
     String inputPassword = "hilbwqeq8432h;3pf23j94p";
     String comparePassword = "ioyfiblewnowrewrpnive";
     byte[] testSalt = "giueofuiwe45687y38hxftxb".getBytes();
 
-    ResultSet rs = null;
     byte[] hash = Security.encodePassword(inputPassword, testSalt);
 
     Boolean passwordMatched = Security.matchPassword(comparePassword, hash, testSalt);
@@ -46,13 +44,12 @@ class SecurityTest {
   }
 
   @Test
-  @Description("Validation: Test password encoding and matching must use same salt")
+  @Description("36 Defect: Test password encoding and matching must use same salt")
   void encodePasswordSaltMustMatch() {
     String testPassword = "hilbwqeq8432h;3pf23j94p";
     byte[] inputSalt = "giueofuiwe45687y38hxftxb".getBytes();
     byte[] compareSalt = "iof3w4".getBytes();
 
-    ResultSet rs = null;
     byte[] hash = Security.encodePassword(testPassword, inputSalt);
 
     Boolean passwordMatched = Security.matchPassword(testPassword, hash, compareSalt);
@@ -61,7 +58,7 @@ class SecurityTest {
   }
 
   @Test
-  @Description("Boundary test: Min number of username characters")
+  @Description("37 Boundary test: Min number of username characters")
   void validateTextBoundaryMinNumChars() {
     String testUsername = "hded";
     Boolean passed = Security.validateText(testUsername);
@@ -70,7 +67,7 @@ class SecurityTest {
   }
 
   @Test
-  @Description("Boundary test: Max number of username characters")
+  @Description("38 Boundary test: Max number of username characters")
   void validateTextBoundaryMaxNumChars() {
     String testUsername = "12345678901234567890";
     Boolean passed = Security.validateText(testUsername);
@@ -79,7 +76,7 @@ class SecurityTest {
   }
 
   @Test
-  @Description("Boundary test: One too few characters in username")
+  @Description("39 Boundary test: One too few characters in username")
   void validateTextBoundaryTooFewChars() {
     String testUsername = "hde";
     Boolean passed = Security.validateText(testUsername);
@@ -88,7 +85,7 @@ class SecurityTest {
   }
 
   @Test
-  @Description("Boundary test: One too many characters in username")
+  @Description("40 Boundary test: One too many characters in username")
   void validateTextBoundaryTooManyChars() {
     String testUsername = "123456789012345678901";
     Boolean passed = Security.validateText(testUsername);
